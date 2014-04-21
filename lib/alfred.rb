e3c93fcbf2c879c3f55c06ab0d9267801789c0d6
DerDerwish <derderwish@metameute.de>
2014-04-21 20:45:53 +0200

    comments added

--- lib/alfred.rb
+++ lib/alfred.rb
@@ -1,6 +1,7 @@
 class Alfred
   attr_accessor :filename, :filetype, :graph
   
+  # get new instance
   def initialize(filename, filetype=File.extname(filename))
     raise ArgumentError, 'Argument is empty' unless not filename.empty?
     raise ArgumentError, 'File ist not available' unless File.exists?(filename)
@@ -8,6 +9,7 @@ class Alfred
     @filetype = filetype
   end
   
+  # read file and return the updated graph
   def update()
     content = ""
     file = File.open(@filename, "r") do |f|
@@ -22,10 +24,12 @@ class Alfred
     end
   end
   
+  # intern method to read from dot-file
   def parse_dot(data)
-    
+    # not yet implemented
   end
   
+  # intern method to read from json-file
   def parse_json(data)
     graph = Graph.new
     primaries = []
@@ -52,21 +56,23 @@ end
 class Graph
   attr_accessor :nodes
   
+  #get new graph
   def initialize
     self.nodes = []
   end
   
+  #add a node to the graph
   def add_node(mac_address)
     self.nodes.push(Node.new(mac_address))
   end
   
+  #get the node with given mac-address
   def get_node_by_mac_address(mac_address)
     self.nodes.each do |node|
       if node.get_mac_addresses().include?(mac_address) then
         return node
       end
     end
-    
     return nil
   end
   
@@ -74,7 +80,8 @@ end
 
 class Node
   attr_accessor :primary_mac_address, :secondary_mac_addresses, :clients, :neighbours
-      
+
+  #add new node with given mac-address to graph
   def initialize(mac_address)
     self.primary_mac_address = mac_address
     self.clients = []
@@ -82,25 +89,30 @@ class Node
     self.secondary_mac_addresses = []
   end
   
+  #get primary mac-address
   def get_primary_mac_address()
     return self.primary_mac_address
   end
   
+  #get all mac-addresses
   def get_mac_addresses()
     addresses = self.secondary_mac_addresses.clone
     addresses.push(self.primary_mac_address)
     return addresses
   end
   
+  #add a new client with given mac-address to the node
   def add_client(mac_address)
     self.clients.push(Client.new(mac_address))
   end
   
+  #add a new connection to the node
   def add_neighbour(mac_address)
     self.neighbours.push(mac_address)
   end
   
 end
+
 class Client
   attr_accessor :mac_address
 end
